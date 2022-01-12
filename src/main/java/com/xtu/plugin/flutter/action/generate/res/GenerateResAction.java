58195a51d8665a4bbd734b377c4d19ed233dccf5
YangLang <yangl@inke.cn>
2022-01-12 21:00:58 +0800

    优化强制生成资源功能

--- src/main/java/com/xtu/plugin/flutter/action/generate/res/GenerateResAction.java
+++ src/main/java/com/xtu/plugin/flutter/action/generate/res/GenerateResAction.java
@@ -4,9 +4,16 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.xtu.plugin.flutter.component.assets.code.DartRFileGenerator;
+import com.xtu.plugin.flutter.utils.FileUtils;
 import com.xtu.plugin.flutter.utils.PluginUtils;
 import com.xtu.plugin.flutter.utils.PubspecUtils;
+import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class GenerateResAction extends AnAction {
 
@@ -14,16 +21,37 @@ public class GenerateResAction extends AnAction {
     public void update(@NotNull AnActionEvent e) {
         Project project = e.getProject();
         if (PluginUtils.isNotFlutterProject(project)) {
-            e.getPresentation().setEnabled(false);
+            e.getPresentation().setVisible(false);
             return;
         }
-        e.getPresentation().setEnabled(true);
+        e.getPresentation().setVisible(true);
     }
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         Project project = e.getProject();
-        if (project == null) return;
-        PubspecUtils.readAssetAtReadAction(project, assetList -> DartRFileGenerator.getInstance().generate(project, assetList));
+        String projectPath = PluginUtils.getProjectPath(project);
+        if (StringUtils.isEmpty(projectPath)) return;
+        List<File> assetFileList = new ArrayList<>();
+        List<String> assetFoldNameList = PluginUtils.supportAssetFoldName(project);
+        for (String assetFoldName : assetFoldNameList) {
+            File assetDirectory = new File(projectPath, assetFoldName);
+            FileUtils.scanDirectory(assetDirectory, assetFileList::add);
+        }
+        List<String> newAssetNameList = new ArrayList<>();
+        for (File assetFile : assetFileList) {
+            String relativePath = FileUtils.getRelativePath(projectPath, assetFile.getAbsolutePath());
+            newAssetNameList.add(relativePath);
+        }
+        //keep asset list order
+        Collections.sort(newAssetNameList);
+        PubspecUtils.readAssetAtReadAction(project, assetList -> {
+            //force refresh R file
+            if (newAssetNameList.equals(assetList)) {
+                DartRFileGenerator.getInstance().generate(project, assetList);
+            } else { //update asset
+                PubspecUtils.writeAssetAtWriteAction(project, newAssetNameList);
+            }
+        });
     }
 }

d78e1a4e4ff2162af627871c18e33247a932cd0e
YangLang <yangl@inke.cn>
2022-02-26 21:44:51 +0800

    fix .DS_Store导致生成_res.dart异常

--- src/main/java/com/xtu/plugin/flutter/action/generate/res/GenerateResAction.java
+++ src/main/java/com/xtu/plugin/flutter/action/generate/res/GenerateResAction.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.xtu.plugin.flutter.component.assets.code.DartRFileGenerator;
+import com.xtu.plugin.flutter.utils.CollectionUtils;
 import com.xtu.plugin.flutter.utils.FileUtils;
 import com.xtu.plugin.flutter.utils.PluginUtils;
 import com.xtu.plugin.flutter.utils.PubspecUtils;
@@ -36,14 +37,21 @@ public class GenerateResAction extends AnAction {
         List<String> assetFoldNameList = PluginUtils.supportAssetFoldName(project);
         for (String assetFoldName : assetFoldNameList) {
             File assetDirectory = new File(projectPath, assetFoldName);
-            FileUtils.scanDirectory(assetDirectory, assetFileList::add);
+            FileUtils.scanDirectory(assetDirectory, (file -> {
+                String fileName = file.getName();
+                //修复隐藏文件导致，生成的res.dart文件异常，如mac下的.DS_Store文件
+                if (!fileName.startsWith(".")) {
+                    assetFileList.add(file);
+                }
+            }));
         }
         List<String> newAssetNameList = new ArrayList<>();
         for (File assetFile : assetFileList) {
             String relativePath = FileUtils.getAssetPath(projectPath, assetFile);
             newAssetNameList.add(relativePath);
         }
-        //keep asset list order
+        //duplicate and sort resource
+        CollectionUtils.duplicateList(newAssetNameList);
         Collections.sort(newAssetNameList);
         PubspecUtils.readAssetAtReadAction(project, assetList -> {
             //force refresh R file

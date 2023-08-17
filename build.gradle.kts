04db5f00f1a09e6889daec5d27c1f7b40d18ba9b
YangLang <yanglang116@gmail.com>
2023-08-17 22:52:40 +0800

    Revert "升级Gradle编译工具"
    This reverts commit d609b25f5cd6b1d8a7d7c459e492c6201d0aa456.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,9 @@ plugins {
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.5.30"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.15.0"
+    id("org.jetbrains.intellij") version "1.3.1"
     // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "2.1.2"
+    id("org.jetbrains.changelog") version "1.3.0"
     // Gradle Qodana Plugin
     id("org.jetbrains.qodana") version "0.1.12"
 }

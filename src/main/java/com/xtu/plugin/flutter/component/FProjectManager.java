0dac528b23576041b2476f9a39fa4f0cedd6ff03
YangLang <yanglang116@gmail.com>
2023-08-17 22:14:26 +0800

    调整项目打开监听方式

--- src/main/java/com/xtu/plugin/flutter/component/FProjectManager.java
+++ src/main/java/com/xtu/plugin/flutter/component/FProjectManager.java
@@ -1,8 +1,7 @@
 package com.xtu.plugin.flutter.component;
 
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.project.ProjectManagerListener;
 import com.xtu.plugin.flutter.action.mock.manager.HttpMockManager;
 import com.xtu.plugin.flutter.component.assets.AssetsManager;
 import com.xtu.plugin.flutter.component.packages.update.FlutterPackageUpdater;
@@ -11,20 +10,15 @@ import com.xtu.plugin.flutter.upgrader.UpgradeManager;
 import com.xtu.plugin.flutter.utils.LogUtils;
 import org.jetbrains.annotations.NotNull;
 
-public class FStartupActivityImpl implements StartupActivity, Disposable {
+public class FProjectManager implements ProjectManagerListener {
 
     @Override
-    public void runActivity(@NotNull Project project) {
-        LogUtils.info("FStartupActivityImpl open Project");
+    public void projectOpened(@NotNull Project project) {
+        LogUtils.info("FProjectManager projectOpened");
         AssetRegisterStorageService.getService(project).refreshIfNeed();
         AssetsManager.getService(project).attach();
         HttpMockManager.getService(project).activeServer();
         FlutterPackageUpdater.getService(project).attach();
         UpgradeManager.checkPluginVersion(project);
-    }
-
-    @Override
-    public void dispose() {
-        LogUtils.info("FStartupActivityImpl close Project");
     }
 }

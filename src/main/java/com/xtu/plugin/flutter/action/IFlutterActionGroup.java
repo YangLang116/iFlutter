a3778de4525d585de1343a55cf6f8c896f5f63a4
YangLang116 <1004145468@qq.com>
2024-07-23 18:25:06 +0800

    添加ActionGroup

---
+++ src/main/java/com/xtu/plugin/flutter/action/IFlutterActionGroup.java
@@ -0,0 +1,23 @@
+package com.xtu.plugin.flutter.action;
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.project.Project;
+import com.xtu.plugin.flutter.utils.PluginUtils;
+import org.jetbrains.annotations.NotNull;
+
+public class IFlutterActionGroup extends DefaultActionGroup {
+
+    @Override
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+        return ActionUpdateThread.BGT;
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        Project project = e.getProject();
+        boolean isFlutterProject = PluginUtils.isFlutterProject(project);
+        e.getPresentation().setVisible(isFlutterProject);
+    }
+}
--- src/main/java/com/xtu/plugin/flutter/action/intl/BaseIntlAction.java
+++ src/main/java/com/xtu/plugin/flutter/action/intl/BaseIntlAction.java
@@ -1,27 +1,21 @@
 package com.xtu.plugin.flutter.action.intl;
 
-import com.intellij.openapi.actionSystem.ActionUpdateThread;
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.xtu.plugin.flutter.action.intl.utils.IntlUtils;
+import com.xtu.plugin.flutter.base.action.FlutterProjectAction;
 import com.xtu.plugin.flutter.utils.CollectionUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-public abstract class BaseIntlAction extends AnAction {
+public abstract class BaseIntlAction extends FlutterProjectAction {
 
     @Override
-    public void update(@NotNull AnActionEvent e) {
+    public void whenUpdate(@NotNull AnActionEvent e) {
         Project project = e.getProject();
         List<String> localeList = IntlUtils.getLocaleList(project);
         boolean hasLocale = !CollectionUtils.isEmpty(localeList);
         e.getPresentation().setVisible(hasLocale);
-    }
-
-    @Override
-    public @NotNull ActionUpdateThread getActionUpdateThread() {
-        return ActionUpdateThread.BGT;
     }
 }
